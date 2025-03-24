0b8b4c354121d6bc3ce454ae44bce8799ff191c7
ilonivna <147661751+ilonivna@users.noreply.github.com>
2025-03-24 19:50:44 +0300

    mw and server setup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
-const message = 'Hello Killua';
+import express from "express";
 
-console.log(message);
+const app = express();
+
+const PORT = 3000;
+
+
+
+//built-in Express MW for parsing request body
+app.use(express.json());
+
+//MW for time logging
+app.use((req,res,next)=>{
+    console.log(`Date: ${new Date().toLocaleString()}`);
+    next();
+});
+
+//request handler for GET requests at path "/"
+app.get("/", (req,res) => {
+    res.json({
+        message: "Hello there Pigg"
+    });
+});
+
+//Error MW
+app.use((err, req, res, next) => {
+    res.status(500).json({
+        message: "Something went really wrong..."
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on PORT ${PORT}`);
+});
